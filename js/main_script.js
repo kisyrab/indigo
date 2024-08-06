a839365d3aeca4fa211cedc87e4e4d5b3f13cb3c
kisyrab <906ksy@naver.com>
2024-08-06 17:44:09 +0900

    8/6 commit

--- js/main_script.js
+++ js/main_script.js
@@ -4,5 +4,38 @@ $(function(){
         dots:true,
         arrows:true,
         fade:true,
+
+        responsive:[
+            {
+                breakpoint : 1000,
+                settings : {
+                    dots:false,
+                    arrows:false
+                }
+            },
+            {
+                breakpoint : 600,
+                settings : {
+                    dots:true,
+                    arrows:true
+                }
+
+            }
+        ]
+
     })
+
+    $(".menu_toggle_btn").click(function(){
+        $(this).toggleClass("on")
+        $(".m_nav").toggleSlide();
+    })
+
+    $("a").click(function(event){
+        event.preventDefault()
+    })
+
+
+
+
+
 })//j
